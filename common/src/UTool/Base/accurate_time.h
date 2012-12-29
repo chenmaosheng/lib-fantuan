7b049400ad250530de4d76559d1e40b472e5a852
chanmaosheng <chanmaosheng@gmail.com>
2012-12-29 02:51:33 +0000

    1, add accurate time 2, add assert 3, add smart pointer
    all of them do not test on linux, let's do it
    
    vet: me

---
+++ common/src/UTool/Base/accurate_time.h
@@ -0,0 +1,45 @@
+#ifndef _H_ACCURATE_TIME
+#define _H_ACCURATE_TIME
+
+#include "type.h"
+#include "singleton.h"
+#ifdef WIN32
+	#include "win32_base.h"
+#endif
+
+namespace Fantuan
+{
+
+class AccurateTime : public Singleton<AccurateTime>
+{
+public:
+	AccurateTime()
+	{
+#ifdef WIN32
+		QueryPerformanceFrequency((_LARGE_INTEGER*)&m_iPerformanceFrequency);
+#endif
+	}
+
+	inline uint64 GetTime()
+	{
+		uint64 time = 0;
+#ifdef WIN32
+		QueryPerformanceCounter((LARGE_INTEGER*)&time);
+		time = time * 1000000000 / m_iPerformanceFrequency;
+#else
+		timespec timeSpec;
+		clock_gettime(CLOCK_MONOTONIC, &timeSpec);
+		time = (uint64)timeSpec.tv.sec * 1000000000 + timeSpec.tv_nsec;
+#endif
+		return time;
+	}
+
+private:
+	uint64	m_iPerformanceFrequency;
+};
+
+}
+
+#define GET_TIME()	Fantuan::AccurateTime::Instance()->GetTime()
+
+#endif
--- common/src/UTool/Base/linux_base.h
+++ common/src/UTool/Base/linux_base.h
@@ -3,12 +3,9 @@
 
 #include <pthread.h>
 #include <semaphore.h>
-#include <errno.h>
-#include <sys/types.h>
 #include <sys/time.h>
 #include <dlfcn.h>
 #include <unistd.h>
-#include <time.h>
 
 #endif
 
