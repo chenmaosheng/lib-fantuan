f29d7f75320cff2f1a6f15d50c9386f834a4cb12
chanmaosheng@gmail.com <chanmaosheng@gmail.com@c9b7ea7f-6296-e214-e44a-18fbc8dc2d9a>
2013-01-03 16:52:15 +0000

    fix a bug about allocator, not fully finished
    vet: me

--- trunk/common/src/UTool/Memory/allocator.h
+++ trunk/common/src/UTool/Memory/allocator.h
@@ -6,8 +6,6 @@
 namespace Fantuan
 {
 
-
-template<size_t N>
 class DefaultAllocator
 {
 	enum { ALIGNMENT = 8, MAX_BYTES = 256, NUM_LIST = 32, };
@@ -16,10 +14,17 @@ class DefaultAllocator
 	{
 		struct Object*	free_list_link;
 		size_t			obj_size;
+		char			data[1];
 	};
 
-	static size_t		freelist_index(size_t bytes);
-	static void*		refill(size_t bytes);
+	enum { OBJECT_OFFSET = sizeof(Object*) + sizeof(size_t), };
+
+	static size_t		freelist_index(size_t bytes)
+	{
+		return (bytes + ALIGNMENT - 1) / ALIGNMENT - 1;
+	}
+
+	static char*		refill(size_t bytes);
 	static char*		chunk_alloc(size_t bytes, int& num_object);
 
 public:
@@ -31,199 +36,137 @@ public:
 private:
 	static Object* volatile
 						m_pFreeList[NUM_LIST];
-	static char*		m_pStartFree;
-	static char*		m_pEndFree;
-	static size_t		m_iTotalSize;
+	static char*		m_pStartFree[NUM_LIST];
+	static char*		m_pEndFree[NUM_LIST];
+	static size_t		m_iTotalSize[NUM_LIST];
 };
 
-template<size_t N>
-char* DefaultAllocator<N>::m_pStartFree = 0;
+char* DefaultAllocator::m_pStartFree[DefaultAllocator::NUM_LIST] = 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-template<size_t N>
-char* DefaultAllocator<N>::m_pEndFree = 0;
+char* DefaultAllocator::m_pEndFree[DefaultAllocator::NUM_LIST] = 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-template<size_t N>
-size_t DefaultAllocator<N>::m_iTotalSize = 0;
+size_t DefaultAllocator::m_iTotalSize[DefaultAllocator::NUM_LIST] = 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-template<size_t N>
-typename DefaultAllocator<N>::Object* volatile 
-DefaultAllocator<N>::m_pFreeList[
-	DefaultAllocator<N>::NUM_LIST
-] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+DefaultAllocator::Object* volatile 
+DefaultAllocator::m_pFreeList[DefaultAllocator::NUM_LIST] = 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-typedef DefaultAllocator<0> FT_Alloc;
+typedef DefaultAllocator FT_Alloc;
 
-template<size_t N>
-size_t DefaultAllocator<N>::freelist_index(size_t bytes)
+void* DefaultAllocator::allocate(size_t bytes)
 {
-	return (bytes + ALIGNMENT - 1) / ALIGNMENT - 1;
-}
-
-template<size_t N>
-void* DefaultAllocator<N>::allocate(size_t n)
-{
-	void* ret = 0;
-	if (n > MAX_BYTES)
+	if (bytes > MAX_BYTES)
 	{
-		ret = malloc(n);
+		return malloc(bytes);
 	}
-	else
-	{
-		Object* volatile* freelist = m_pFreeList + freelist_index(n);
+
+	bytes += OBJECT_OFFSET;
 		
-		//AutoLocker locker(&m_Mutex);
-		Object* result = *freelist;
-		if (!result)
-		{
-			ret = refill(ROUND_UP(n, ALIGNMENT));
-		}
-		else
-		{
-			*freelist = result->free_list_link;
-			ret = result;
-		}
-	}
-
-	return ret;
-}
-
-template<size_t N>
-void DefaultAllocator<N>::deallocate(void* ptr, size_t n)
-{
-	if (n > MAX_BYTES)
+	Object* volatile* freelist = m_pFreeList + freelist_index(bytes);
+		
+	//AutoLocker locker(&m_Mutex);
+	Object* pObject = *freelist;
+	if (!pObject)
 	{
-		free(ptr);
-	}
-	else
-	{
-		Object* volatile* freelist = m_pFreeList + freelist_index(n);
-		Object* temp = (Object*)ptr;
-
-		//AutoLocker locker(&m_Mutex);
-		temp->free_list_link = *freelist;
-		*freelist = temp;
-	}
-}
-
-template<size_t N>
-void DefaultAllocator<N>::deallocate(void* ptr)
-{
-	Object* temp = (Object*)ptr;
-	if (temp->obj_size > MAX_BYTES)
-	{
-		free(ptr);
-	}
-	else
-	{
-		Object* volatile* freelist = m_pFreeList + freelist_index(temp->obj_size);
-
-		//AutoLocker locker(&m_Mutex);
-		temp->free_list_link = *freelist;
-		*freelist = temp;
-	}
-}
-
-template<size_t N>
-void* DefaultAllocator<N>::reallocate(void* ptr, size_t old_size, size_t new_size)
-{
-	void* result;
-	size_t copy_size;
-
-	if (old_size > MAX_BYTES && new_size > MAX_BYTES)
-	{
-		return realloc(ptr, new_size);
-	}
-
-	if (ROUND_UP(old_size, ALIGNMENT) == ROUND_UP(new_size, ALIGNMENT))
-	{
-		return ptr;
-	}
-
-	result = allocate(new_size);
-	copy_size = new_size > old_size ? old_size : new_size;
-	memcpy(result, ptr, copy_size);
-	deallocate(ptr, old_size);
-	return result;
-}
-
-template<size_t N>
-char* DefaultAllocator<N>::chunk_alloc(size_t size, int &_nobject)
-{
-	char* result;
-	size_t total_bytes = size * _nobject;
-	size_t bytes_left = m_pEndFree - m_pStartFree;
-	
-	if (bytes_left >= total_bytes)
-	{
-		result = m_pStartFree;
-		m_pStartFree += total_bytes;
-		return result;
-	}
-	else if (bytes_left >= size)
-	{
-		_nobject = (int)(bytes_left/size);
-		total_bytes = size * _nobject;
-		result = m_pStartFree;
-		m_pStartFree += total_bytes;
-		return result;
-	}
-
-	size_t new_bytes = total_bytes * 4;// + ROUND_UP((m_iTotalSize >> 8), ALIGNMENT);
-
-	if (bytes_left > 0)
-	{
-		Object* volatile* freelist = m_pFreeList + freelist_index(bytes_left);
-		((Object*)m_pStartFree)->free_list_link = *freelist;
-		*freelist = (Object*)m_pStartFree;
-	}
-
-	printf("start malloc, new_bytes: %d\n", new_bytes);
-	try
-	{
-		m_pStartFree = (char*)malloc(new_bytes);
-	}
-	catch (...)
-	{
-		printf("bad malloc\n");	
+		// empty freelist
+		return refill(ROUND_UP(bytes, ALIGNMENT));
 	}
 	
-	printf("end malloc\n");
-	m_iTotalSize += new_bytes;
-	printf("total: %d, new: %d, align: %d\n", m_iTotalSize, new_bytes, new_bytes - total_bytes * 2);
-	m_pEndFree = m_pStartFree + new_bytes;
-	return chunk_alloc(size, _nobject);
+	// set new freelist
+	*freelist = pObject->free_list_link;
+	return pObject->data;
 }
 
-template<size_t N>
-void* DefaultAllocator<N>::refill(size_t size)
+void DefaultAllocator::deallocate(void* ptr, size_t bytes)
+{
+	if (bytes > MAX_BYTES)
+	{
+		free(ptr);
+		return;
+	}
+
+	Object* volatile* freelist = m_pFreeList + freelist_index(bytes);
+	Object* pObject = (Object*)ptr;
+
+	//AutoLocker locker(&m_Mutex);
+	pObject->free_list_link = *freelist;
+	*freelist = pObject;
+}
+
+void DefaultAllocator::deallocate(void* ptr)
+{
+	Object* pObject = (Object*)((char*)ptr - FIELD_OFFSET(Object, data));
+	deallocate(ptr, pObject->obj_size);
+}
+
+char* DefaultAllocator::chunk_alloc(size_t bytes, int &_nobject)
+{
+	char* chunkMem;
+	size_t index = freelist_index(bytes);
+	size_t total_bytes = bytes * _nobject;
+	size_t left_bytes = m_pEndFree[index] - m_pStartFree[index];
+	
+	if (left_bytes >= total_bytes)
+	{
+		chunkMem = m_pStartFree[index];
+		m_pStartFree[index] += total_bytes;
+		return chunkMem;
+	}
+	
+	// can't provide all of them
+	if (left_bytes >= bytes)
+	{
+		_nobject = (int)(left_bytes / bytes);
+		total_bytes = bytes * _nobject;
+		chunkMem = m_pStartFree[index];
+		m_pStartFree[index] += total_bytes;
+		return chunkMem;
+	}
+
+	// ready to enlarge
+	size_t new_bytes = total_bytes * 2 + ROUND_UP((m_iTotalSize[index] >> 4), ALIGNMENT);
+	m_pStartFree[index] = (char*)malloc(new_bytes);
+	
+	m_iTotalSize[index] += new_bytes;
+	m_pEndFree[index] = m_pStartFree[index] + new_bytes;
+	return chunk_alloc(bytes, _nobject);
+}
+
+char* DefaultAllocator::refill(size_t bytes)
 {
 	int _nobject = 20;
-	char* chunk = chunk_alloc(size, _nobject);
+	char* chunk = chunk_alloc(bytes, _nobject);
 	Object* volatile* freelist;
-	Object* result;
+	Object* pObject;
 	Object* curr_obj;
 	Object* next_obj;
-	int i;
+
+	pObject = (Object*)chunk;
+	pObject->obj_size = bytes;
 
 	if (_nobject == 1)
 	{
-		return chunk;
+		return pObject->data;
 	}
-	freelist = m_pFreeList + freelist_index(size);
-	
-	result = (Object*)chunk;
-	result->obj_size = size;
-	*freelist = next_obj = (Object*)(chunk + size);
-	next_obj->obj_size = size;
-	for (i = 1; ; i++)
+
+	freelist = m_pFreeList + freelist_index(bytes);
+	*freelist = next_obj = (Object*)(chunk + bytes);
+	next_obj->obj_size = bytes;
+	for (int i = 1; ; i++)
 	{
 		curr_obj = next_obj;
-		next_obj = (Object*)((char*)next_obj + size);
-		next_obj->obj_size = size;
+		next_obj = (Object*)((char*)next_obj + bytes);
+		next_obj->obj_size = bytes;
 		if (_nobject - 1 == i)
 		{
-			curr_obj->obj_size = size;
+			curr_obj->obj_size = bytes;
 			curr_obj->free_list_link = 0;
 			break;
 		}
@@ -231,37 +174,8 @@ void* DefaultAllocator<N>::refill(size_t size)
 		curr_obj->free_list_link = next_obj;
 	}
 
-	return result;
+	return pObject->data;
 }
-
-template<typename T>
-class allocator
-{
-	typedef FT_Alloc _Alloc;
-
-public:
-	allocator() {}
-	allocator(const allocator&) {}
-	template<typename T>
-	allocator(const allocator<T>&) {}
-	~allocator() {}
-
-	T* allocate(size_t n, const void* = 0)
-	{
-		return n != 0 ? static_cast<T*>(
-			_Alloc::allocate(n * sizeof(T))) : 0;
-	}
-
-	void deallocate(T* ptr, size_t n)
-	{
-		_Alloc::deallocate(ptr, n * sizeof(T));
-	}
-
-	void deallocate(T* ptr)
-	{
-		_Alloc::deallocate(ptr);
-	}
-};
 
 }
 
