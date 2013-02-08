23e589815353848c758f9ad52b167d0a1df8927f
chanmaosheng <chanmaosheng@gmail.com>
2013-02-08 14:11:31 +0000

    fix a bug(need to know more about template in cpp file)
    vet: me

--- common/src/UTool/DataStructure/string_base.h
+++ common/src/UTool/DataStructure/string_base.h
@@ -71,6 +71,30 @@ private:
 	ArrayBase<T>* m_pData;
 };
 
+template<>
+size_t	StringBase<char>::strlen()	const
+{
+	return ::strlen(m_pData->data());
+}
+
+template<>
+size_t	StringBase<char>::strlen(const char* pData)	const
+{
+	return ::strlen(pData);
+}
+
+template<>
+size_t	StringBase<wchar_t>::strlen()	const
+{
+	return ::wcslen(m_pData->data());
+}
+
+template<>
+size_t	StringBase<wchar_t>::strlen(const wchar_t* pData)	const
+{
+	return ::wcslen(pData);
+}
+
 
 }
 
