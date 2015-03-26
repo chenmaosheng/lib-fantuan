8a5abd23c64aa586744dc2ce9c29985c16fd1f6c
chanmaosheng <chanmaosheng@gmail.com>
2015-03-26 07:02:01 +0000

    add semaphore support to secure threads

--- easynet/easy_connection.cpp
+++ easynet/easy_connection.cpp
@@ -4,9 +4,12 @@
 
 EasyConnection::EasyConnection(EasyAcceptor* pAcceptor) : acceptor_(pAcceptor)
 {
-	recv_context_ = new EasyContext(OPERATION_RECV, this);
+	recv_context_ = new EasyContext;
 	recv_context_->buffer_ = (char*)malloc(1024);
-	send_context_ = new EasyContext(OPERATION_SEND, this);
+	send_context_ = new EasyContext;
+	send_context_plus_ = new EasyContext;
+	send_context_plus_->buffer_ = (char*)malloc(1024*64);
+	sem_init(&sem_, 0, 1);
 }
 
 int EasyConnection::HandleMessage()
@@ -69,14 +72,14 @@ int EasyConnection::HandleMessage()
 
 int EasyConnection::SendMessage()
 {
-	if (send_context_->len_ == 0)
+	if (send_context_plus_->len_ == 0)
 	{
 		return 0;
 	}
 	printf("send the left\n");
 	int writeNum = 0;
-	int total = send_context_->len_;
-	char* p = send_context_->buffer_;
+	int total = send_context_plus_->len_;
+	char* p = send_context_plus_->buffer_;
 	bool bWriteOK = false;
 	while (true)
 	{
@@ -123,17 +126,20 @@ int EasyConnection::SendMessage()
 
 	if (bWriteOK)
 	{
-		send_context_->buffer_[0] = '\0';
-		send_context_->len_ = 0;
+		send_context_plus_->buffer_[0] = '\0';
+		send_context_plus_->len_ = 0;
 		ev_.events = EPOLLIN | EPOLLET;
 		epoll_ctl(acceptor_->epfd_, EPOLL_CTL_MOD, socket_, &ev_);
 	}
+
+	sem_post(&sem_);
 
 	return 0;
 }
 
 int EasyConnection::SendMessage(char* buffer, int len)
 {
+	sem_wait(&sem_);
 	bool bWriteOK = false;
 	int writeNum = 0;
 	send_context_->buffer_ = buffer;
@@ -148,9 +154,11 @@ int EasyConnection::SendMessage(char* buffer, int len)
 			if (errno == EAGAIN)
 			{
 				printf("sendmessage, eagain, go\n");
+				memcpy(send_context_plus_->buffer_, send_context_->buffer_, send_context_->len_);
+				send_context_plus_->len_ = send_context_->len_;
 				ev_.events = EPOLLOUT | EPOLLET;
 				epoll_ctl(acceptor_->epfd_, EPOLL_CTL_MOD, socket_, &ev_);
-				break;
+				return 0;
 			}
 		}
 		else if (writeNum == 0)
@@ -178,6 +186,8 @@ int EasyConnection::SendMessage(char* buffer, int len)
 		send_context_->buffer_[0] = '\0';
 		send_context_->len_ = 0;
 	}
+
+	sem_post(&sem_);
 
 	return 0;
 }
