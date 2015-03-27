68ff81e898f00e243320f0ec0e277826f087bb96
chanmaosheng <chanmaosheng@gmail.com>
2015-03-27 09:04:37 +0000

    make acceptor easier to use

--- EasyNetwork/Test/main.cpp
+++ EasyNetwork/Test/main.cpp
@@ -6,11 +6,7 @@ int main(int argc, char* argv[])
 {
 	EasyNetwork::Init();
 	EasyWorker* pWorker = EasyWorker::CreateWorker(2);
-	SOCKADDR_IN addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(9001);
-	EasyAcceptor* pAcceptor = EasyAcceptor::CreateAcceptor(&addr, pWorker);
+	EasyAcceptor* pAcceptor = EasyAcceptor::CreateAcceptor(0, 9001, pWorker);
 	pAcceptor->Start();
 
 	while (true)
