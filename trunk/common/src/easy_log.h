a57dbb5eb2a799f3959e5cbbf73b638596b839e7
chanmaosheng@gmail.com <chanmaosheng@gmail.com@c9b7ea7f-6296-e214-e44a-18fbc8dc2d9a>
2015-03-29 04:33:21 +0000

    make easylog available in linux mode

--- trunk/common/src/easy_log.h
+++ trunk/common/src/easy_log.h
@@ -19,40 +19,40 @@ public:
 	void	Init(int32 iLogLevel = LOG_DEBUG_LEVEL);
 	void	Destroy();
 	void	AddLogDevice(EasyLogDevice*);
-	bool	Push(int32 iLogLevel, wchar_t* strFormat, ...);
+	bool	Push(int32 iLogLevel, const wchar_t* strFormat, ...);
 	int32	GetLogLevel() const
 	{
 		return m_iLogLevel;
 	}	
 
-	wchar_t*	_Level2String(int32 iLogLevel);
-	static uint32 __stdcall _LogOutput(void*);
-	void _Tick();
-	void _Output(wchar_t* strBuffer, uint16 iCount);
+	const wchar_t*	_Level2String(int32 iLogLevel);
+	static uint32	_LogOutput(void*);
+	void			_Tick();
+	void			_Output(wchar_t* strBuffer, uint16 iCount);
 
 private:
 	wchar_t			m_strBuffer[BUFFER_MAX_SIZE];
 	EasyBuffer*		m_pEasyBuffer;
 	int32			m_iLogLevel;
 	std::mutex		m_Mutex;
-	HANDLE			m_OutputEvent;
+	std::condition_variable	m_OutputEvent;
 	std::thread*	m_pThread;
 };
 
 #define LOG_DBG(Expression, ...)										\
 	if (EasyLog::Instance()->GetLogLevel() <= EasyLog::LOG_DEBUG_LEVEL)					\
-	EasyLog::Instance()->Push(EasyLog::LOG_DEBUG_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, GetCurrentThreadId(), _T(__FUNCTION__), __LINE__, ##__VA_ARGS__);
+	EasyLog::Instance()->Push(EasyLog::LOG_DEBUG_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, EASY_THREADID, FUNCTION_NAME, __LINE__, ##__VA_ARGS__);
 
 #define LOG_WAR(Expression, ...)										\
 	if (EasyLog::Instance()->GetLogLevel() <= EasyLog::LOG_WARNING_LEVEL)				\
-	EasyLog::Instance()->Push(EasyLog::LOG_WARNING_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, GetCurrentThreadId(), _T(__FUNCTION__), __LINE__, ##__VA_ARGS__);
+	EasyLog::Instance()->Push(EasyLog::LOG_WARNING_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, EASY_THREADID, FUNCTION_NAME, __LINE__, ##__VA_ARGS__);
 
 #define LOG_ERR(Expression, ...)										\
 	if (EasyLog::Instance()->GetLogLevel() <= EasyLog::LOG_ERROR_LEVEL)					\
-	EasyLog::Instance()->Push(EasyLog::LOG_ERROR_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, GetCurrentThreadId(), _T(__FUNCTION__), __LINE__, ##__VA_ARGS__);
+	EasyLog::Instance()->Push(EasyLog::LOG_ERROR_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, EASY_THREADID, FUNCTION_NAME, __LINE__, ##__VA_ARGS__);
 
 #define LOG_STT(Expression, ...)										\
 	if (EasyLog::Instance()->GetLogLevel() <= EasyLog::LOG_STATE_LEVEL)				\
-	EasyLog::Instance()->Push(EasyLog::LOG_STATE_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, GetCurrentThreadId(), _T(__FUNCTION__), __LINE__, ##__VA_ARGS__);
+	EasyLog::Instance()->Push(EasyLog::LOG_STATE_LEVEL, _T("[%u:%-30s][LINE:%-4u] ") Expression, EASY_THREADID, FUNCTION_NAME, __LINE__, ##__VA_ARGS__);
 
 #endif
