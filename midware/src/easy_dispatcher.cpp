e08fc0f1a856883e7936a2008455555a66aaf837
chanmaosheng <chanmaosheng@gmail.com>
2015-04-03 04:31:13 +0000

    try to fix a crash in linux mode add a new command: ping

--- midware/src/easy_dispatcher.cpp
+++ midware/src/easy_dispatcher.cpp
@@ -7,6 +7,6 @@ EasyDispatcher::Func* EasyDispatcher::m_pFuncList;
 bool EasyDispatcher::OnPacketReceived(void* pSession, uint16 iTypeId, uint16 iLen, const char *pBuf)
 {
 	LOG_DBG(_T("FuncType=%d Len=%d"), iTypeId, iLen);
-	Stream stream(iLen, pBuf);	// put buffer into stream
+	InputStream stream(iLen, pBuf);	// put buffer into stream
 	return m_pFuncList[iTypeId](pSession, stream);
 }
